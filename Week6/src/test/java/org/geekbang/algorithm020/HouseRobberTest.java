0a948b2f5830b2a210c8fcbda48ded47eec5b9d2
gaoffee <gaoffee@hotmail.com>
2020-12-23 00:01:08 +0800

    house robber

---
+++ Week6/src/test/java/org/geekbang/algorithm020/HouseRobberTest.java
@@ -0,0 +1,14 @@
+package org.geekbang.algorithm020;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class HouseRobberTest {
+
+    @Test
+    public void rob() {
+        assertThat(new HouseRobber().rob(new int[]{1, 2, 3, 1})).isEqualTo(4);
+        assertThat(new HouseRobber().rob(new int[]{2, 7, 9, 3, 1})).isEqualTo(12);
+    }
+}
